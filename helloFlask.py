276f8cd3cf569b432a063db03724985ce678ed95
vazudew <vazudew@gmail.com>
2019-12-06 00:09:37 +0100

    FRONT_END: updated static files
    * added operations
    * added results
    * added cycling of pages

--- helloFlask.py
+++ helloFlask.py
@@ -1,27 +1,29 @@
 from CalculatorClass import CalculatorClass
-from flask import Flask, redirect, url_for, request, jsonify
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 tempCalculatorObject = None
-
+switcher = {
+    "add" : "+",
+    "sub" : "-",
+    "mul" : "*",
+    "div" : "/"
+    }
 
 @app.route('/init',methods = ['POST', 'GET'])
 def init():
    if request.method == 'POST':
-       item1 = request.form['item1input']
-       item2 = request.form['item2input']
-       oper = request.form['Operator']
+       item1 = int(request.form['item1input'])
+       item2 = int(request.form['item2input'])
+       oper = switcher.get(request.form['Operator'])
    else:
-       item1 = request.args.get('item1input')
-       item2 = request.args.get('item2input')
-       oper = request.args.get('Operator')
+       item1 = int(request.args.get('item1input'))
+       item2 = int(request.args.get('item2input'))
+       oper = switcher.get(request.args.get('Operator'))
        print("item1" + str(item1) + " item2 " + str(item2) + " Operator :" + oper)
    tempCalculatorObject = CalculatorClass (item1, item2, oper)
-   return jsonify(
-       item1 = tempCalculatorObject.item1,
-       item2 = tempCalculatorObject.item2,
-       operator = tempCalculatorObject.operator
-   )
+   return render_template('result.html', item1 = str(tempCalculatorObject.item1), item2 = str(tempCalculatorObject.item2),
+                          oper = str(tempCalculatorObject.operator), result = str(tempCalculatorObject.runOperation()))
 
 if __name__ == '__main__':
    app.run()
