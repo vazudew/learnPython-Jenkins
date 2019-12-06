e495422a3420ba1508d5b8e284273aff02320274
vazudew <vazudew@gmail.com>
2019-12-06 23:10:19 +0100

    JENKINS: updated git clone url
    git clone url is pointed towards right repo

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -6,24 +6,18 @@ agent any
 		stage('checkout') {
 				steps {
 					sh 'echo "checkout the code here "'
-					sh 'echo "remove older content from directory"'
-					sh 'pwd'
 					cleanWs()
-					sh 'echo "create working directory again"'
-					sh 'echo "ensure empty folder"'
-					sh 'ls -la'
 					sh 'echo "execute git clone and check the folder"'
-					git credentialsId: '7776e9c9-9379-45e6-a4fd-f97d834f1dda', url: 'https://github.com/vazudew/jenkins_setup.git'
-					sh 'ls -la'
+					git branch: 'master', credentialsId: '7776e9c9-9379-45e6-a4fd-f97d834f1dda', url: 'https://github.com/vazudew/learnPython-Jenkins.git'
 			 }
 		}
 		stage('Test') {
 				steps {
 					sh 'echo "Test Phase"'
-					sh 'pwd'
-					sh 'ls -la'
-					sh 'uname -a'
-					sh 'ifconfig '
+					//sh 'pwd'
+					//sh 'ls -la'
+					//sh 'uname -a'
+					//sh 'ifconfig '
 					sh 'python CalculatorTest.py'
 				}
 			}
