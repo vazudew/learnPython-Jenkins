efbd69b30a9124eb9bcaacaa9dc3d25ab2c03204
vazudew <vazudew@gmail.com>
2019-12-06 19:04:56 +0100

    JENKINS: docker specs added

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,7 +1,11 @@
 #!/usr/bin/env groovy
 
 pipeline {
-agent { docker { image 'python:3.7.2' } }
+agent { 
+		docker { image 'python:3.7.2' 
+		args '-v $HOME:/var/lib/jenkins/workspace/webapp -w /var/lib/jenkins/workspace/webapp -u root'
+		} 
+	}
 		options {
 		skipStagesAfterUnstable()
 	}
@@ -9,7 +13,16 @@ agent { docker { image 'python:3.7.2' } }
 		stage('checkout') {
 				steps {
 					sh 'echo "checkout step here"'
-					//git credentialsId: '7776e9c9-9379-45e6-a4fd-f97d834f1dda', url: 'https://github.com/vazudew/jenkins_setup.git'
+					sh 'echo "remove older content"'
+					sh 'rm -rf /var/lib/jenkins/workspace/webapp'
+					sh 'echo "create working directory again"'
+					sh 'mkdir /var/lib/jenkins/workspace/webapp'
+					dir(path: '/var/lib/jenkins/workspace/webapp')
+					sh 'echo "ensure empty folder"'
+					sh 'ls -la'
+					sh 'echo "execute git clone and check the folder"'
+					git credentialsId: '7776e9c9-9379-45e6-a4fd-f97d834f1dda', url: 'https://github.com/vazudew/jenkins_setup.git'
+					sh 'ls -la'
 			 }
 		}
 		stage('Test') {
