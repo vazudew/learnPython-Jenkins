6591cd151415c62b5dd6deeb5388e20042cabb04
vazudew <vazudew@gmail.com>
2019-12-06 18:52:50 +0100

    JENKINS : commented git pull

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -9,7 +9,7 @@ agent { docker { image 'python:3.7.2' } }
 		stage('checkout') {
 				steps {
 					sh 'echo "checkout step here"'
-					git credentialsId: '7776e9c9-9379-45e6-a4fd-f97d834f1dda', url: 'https://github.com/vazudew/jenkins_setup.git'
+					//git credentialsId: '7776e9c9-9379-45e6-a4fd-f97d834f1dda', url: 'https://github.com/vazudew/jenkins_setup.git'
 			 }
 		}
 		stage('Test') {
